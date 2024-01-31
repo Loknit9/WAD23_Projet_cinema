7f1af151033beb26b24061b5c03e1da89813569a
Laurence Delforge <laurencedelforge@hotmail.com>
2024-01-31 16:26:08 +0100

    asp CinemaPlace suite

---
+++ ASP-cinema/Controllers/CinemaPlaceController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BLL_cinema.Entities;
+using ASP_cinema.Models;
+using ASP_cinema.Handlers;
+using Shared.Repositories;
+
+namespace ASP_cinema.Controllers
+{
+    public class CinemaPlaceController : Controller
+    {
+
+    }
+    
+}
