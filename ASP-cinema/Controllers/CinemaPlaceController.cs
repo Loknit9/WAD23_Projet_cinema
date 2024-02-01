d51dcc0308764b80de056ab89fea991425e4e8ea
Loknit9 <laurencedelforge@hotmail.com>
2024-02-01 14:10:37 +0100

    Controller CinemaPlace ok

--- ASP-cinema/Controllers/CinemaPlaceController.cs
+++ ASP-cinema/Controllers/CinemaPlaceController.cs
@@ -8,7 +8,24 @@ namespace ASP_cinema.Controllers
 {
     public class CinemaPlaceController : Controller
     {
+        private readonly ICinemaPlaceRepository<CinemaPlace> _cinemaPlaceRepository;
+        public CinemaPlaceController(ICinemaPlaceRepository<CinemaPlace> cinemaPlaceRepository)
+        {
+            _cinemaPlaceRepository = cinemaPlaceRepository;
+        }
 
+        // GET: CinemaPlaceController
+        public ActionResult Index()
+        {
+            IEnumerable<CinemaPlaceListItemViewModel> model = _cinemaPlaceRepository.Get().Select(d => d.ToListItem());
+            return View(model);
+        }
+
+        // GET: CinemPlaceController/Details/id
+        public ActionResult Details(int id) 
+        { 
+            return View();
+        }
     }
     
 }
