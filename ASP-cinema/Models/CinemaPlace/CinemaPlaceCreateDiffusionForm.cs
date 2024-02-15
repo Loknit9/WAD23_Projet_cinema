4e0879dabaeff40c4882b7256e9e6bc9bf2c3126
Laurence Delforge <laurencedelforge@hotmail.com>
2024-02-15 15:39:49 +0100

    affichage details

---
+++ ASP-cinema/Models/CinemaPlace/CinemaPlaceCreateDiffusionForm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_cinema.Models.CinemaPlace
+{
+    public class CinemaPlaceCreateDiffusionForm
+    {
+        [HiddenInput]
+        [Required]
+        public int Id_CinemaPlace { get; set; }
+
+        [DisplayName("Nom")]
+        public string Name { get; set; }
+
+        [DisplayName("Ville")]
+        public string City { get; set; }
+    }
+}
